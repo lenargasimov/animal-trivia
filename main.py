b1382ccc31cdc9c7a0ef6b497221c8450156a46c
lenargasimov <lenargasimov@duck.com>
2022-02-22 16:24:18 +0300

    feat: added index url

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from model import db
 
 app = Flask(__name__)
 
 
-@app.route('/questions')
-def questions_view():
-    questions_db = db[0]
+@app.route('/questions/<int:index>')
+def questions_view(index):
+    try:
+        questions_db = db[index]
+        return render_template('quiz.html', question=questions_db)
 
-    return render_template('quiz.html', question=questions_db)
+    except IndexError:
+        abort(404)
+
 
 
 if __name__ == '__main__':
