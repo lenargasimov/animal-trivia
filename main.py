a55450a43e623451f9294a2900668723c3df02b8
lenargasimov <lenargasimov@duck.com>
2022-02-24 21:33:57 +0300

    feat: added add new question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, abort, jsonify
-from model import db
+from flask import Flask, render_template, abort, jsonify, request, redirect, url_for
+from model import db, save_db
 
 app = Flask(__name__)
 
@@ -23,11 +23,6 @@ def questions_view(index):
         abort(404)
 
 
-@app.route('/api/question/')
-def api_question_list():
-    return jsonify(db)
-
-
 @app.route('/api/question/<int:index>')
 def api_question_detail(index):
     try:
@@ -37,6 +32,29 @@ def api_question_detail(index):
         abort(404)
 
 
+@app.route('/add_new_question', methods=['GET', 'POST'])
+def add_question():
 
+    if request.method == 'POST':
+
+        question_dict = {
+            "question": request.form['question'], 
+            "answer": request.form['answer']
+        }
+
+        db.append(question_dict)
+        save_db()
+
+        return redirect(url_for('questions_view', index=len(db) - 1))
+
+    else:
+        return render_template('add_question.html')
+
+
+@app.route('/api/question/')
+def api_question_list():
+    return jsonify(db)
+
+ 
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -2,8 +2,12 @@ import json
 
 
 def load_db():
-    with open("questions.json") as file:
+    with open('questions.json') as file:
         return json.load(file)
 
+
+def save_db():
+    with open('questions.json', 'w') as file:
+        return json.dump(db, file)
 
 db = load_db()
